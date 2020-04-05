c2763c1b9cc863efd46acbaaaa6004b18ca0b610
Radu <39598837+radudd@users.noreply.github.com>
2020-04-05 20:47:13 +0200

    Update ci-dependencies.sh

--- ci-dependencies.sh
+++ ci-dependencies.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 
 curl -O https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/oc/4.3/linux/oc.tar.gz 
-tar xzvf oc.tar.gz -C /usr/local/bin/ 
+sudo tar xzvf oc.tar.gz -C /usr/local/bin/ 
 rm -f oc.tar.gz

3c2751c82813d26a3d89b56293a41be0763ab74a
Radu <39598837+radudd@users.noreply.github.com>
2020-04-05 20:57:29 +0200

    Update ci-dependencies.sh

--- ci-dependencies.sh
+++ ci-dependencies.sh
@@ -2,11 +2,11 @@
 
 # Validate grafana-dashboard
 echo "## Validating Grafana dashboard YAML ##"
-cat grafana-dashboard.json | jq .
+cat grafana-dashboard.json | jq . &>/dev/null || exit 99
 
 # Install OC CLI
 
 echo "## Installing OC CLI ##"
-curl -O https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/oc/4.3/linux/oc.tar.gz 
-sudo tar xzvf oc.tar.gz -C /usr/local/bin/ 
-rm -f oc.tar.gz
+curl -O https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/oc/4.3/linux/oc.tar.gz &>/dev/null || exit 99
+sudo tar xzvf oc.tar.gz -C /usr/local/bin/ || exit 99
+rm -f oc.tar.gz 

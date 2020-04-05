4b189726da624680b431c78a7f99a0a83ad3e218
radu <radu@gogs.io>
2020-04-05 20:16:52 +0200

    added travis deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat grafana-dashboard.json | jq .
+
+oc patch grafanadashboard baby-room-current -p '{"spec": `cat grafana-dashboard.json`}"
+
+openssl aes-256-cbc -K $encrypted_c25bfdc08df8_key -iv $encrypted_c25bfdc08df8_iv -in kubeconfig.enc -out kubeconfig -d
+oc apply -f oc-grafana-dashboard.yaml --config=kubeconfig

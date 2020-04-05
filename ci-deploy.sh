14cff4fdac166be03be6b9192c8ab3e1e5ef5b0e
Radu <39598837+radudd@users.noreply.github.com>
2020-04-05 20:35:49 +0200

    Update ci-deploy.sh

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cat grafana-dashboard.json | jq .
-
 oc patch grafanadashboard baby-room-current -p '{"spec": `cat grafana-dashboard.json`}"
 
 openssl aes-256-cbc -K $encrypted_c25bfdc08df8_key -iv $encrypted_c25bfdc08df8_iv -in kubeconfig.enc -out kubeconfig -d

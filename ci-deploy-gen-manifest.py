9bd9fb38a3ac050e90b9f9e844119f5b16de025e
radu <radu@gogs.io>
2020-04-05 22:57:36 +0200

    some debug for deplot

--- ci-deploy-gen-manifest.py
+++ ci-deploy-gen-manifest.py
@@ -22,6 +22,7 @@ try:
   with open("grafana-dashboard.json", "r") as f:
     grafana_dashboard = json.load(f)
   grafana["spec"]["json"] = grafana_dashboard
+  print(yaml.safe_dump(grafana))
   with open("grafana.json", "w") as f:
     json.dump(grafana, f)
 except OSError as e:
